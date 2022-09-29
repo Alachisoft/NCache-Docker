a71c0142463cb8a2d92fd870b7d1e518604f3b52
DIYATECH\mirza_jahangeer <mirza_jahangeer@diyatech.com>
2022-09-29 15:10:49 +0500

    docker stuff for enterprise client for windows updated

--- enterprise/client/WindowServerCore/resources/CreateScheduledTasks.ps1
+++
@@ -1,2 +0,0 @@
-# Creating One time executing IP Binding task that will change the IP in the NCache Configuration files on the first time the container is started.
-schtasks /create /TN IPBinding /SC ONSTART /TR "c:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe C:\app\IPBinding.ps1" /ru SYSTEM
