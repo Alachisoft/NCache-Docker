9db825b0ea9a9a315e4f6e996086e34a60ebac02
DIYATECH\mirza_jahangeer <mirza_jahangeer@diyatech.com>
2022-09-29 12:37:15 +0500

    docker file for windows and it's ipbinding script is updated according to 5.3 sp1

--- enterprise/server/WindowServerCore/resources/IpBinding.ps1
+++ enterprise/server/WindowServerCore/resources/IpBinding.ps1
@@ -15,19 +15,32 @@ $newIP = $ipaddress = $(ipconfig | where {$_ -match 'IPv4.+\s(\d{1,3}\.\d{1,3}\.
 Write-Host "";
 Write-Output "CHANGING CONFIGURATIONS";
 $path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $config) -Childpath "client.ncconf";
-(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
-
-$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $config) -Childpath "config.ncconf";
-(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
-
-$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $service) -Childpath "Alachisoft.NCache.Service.exe.config";
-(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
-
-$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $service) -Childpath "Alachisoft.NCache.BridgeService.exe.config";
-if(Test-Path $path)
-{
 	(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
-}
+
+	$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $config) -Childpath "config.ncconf";
+	(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
+	
+	$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $config) -Childpath "bridge.ncconf";
+	(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
+
+	$path = Join-Path -Path (Join-Path -Path $installDir -ChildPath $service) -Childpath "Alachisoft.NCache.Service.dll.config";
+	(Get-Content $path) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $path
+
+	$pathToLoader = Join-Path -Path (Join-Path -Path $installDir -ChildPath $service) -Childpath "Alachisoft.NCache.LoaderService.dll.config";
+	$loader_xml = [xml](Get-Content $pathToLoader)
+	$oldLoaderIP = ($loader_xml.configuration.appSettings.add | Where-Object {$_.key -eq "LoaderIP"} | Select value) | Select -ExpandProperty "value"
+	if(Test-Path $pathToLoader)
+	{
+		(Get-Content $pathToLoader) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $pathToLoader
+	}
+	$pathToBridge = Join-Path -Path (Join-Path -Path $installDir -ChildPath $service) -Childpath "Alachisoft.NCache.BridgeService.dll.config";
+	$bridge_xml = [xml](Get-Content $pathToBridge)
+	$oldBridgeIP = ($bridge_xml.configuration.appSettings.add | Where-Object {$_.key -eq "NCacheServer.BindToIP"} | Select value) | Select -ExpandProperty "value"
+	
+	if(Test-Path $pathToBridge)
+	{
+		(Get-Content $pathToBridge) | Foreach-Object {$_ -replace , $oldIP, $newIP}  | Out-File $pathToBridge
+	}
 
 Write-Host "Configurations are modified successfully.";
 
